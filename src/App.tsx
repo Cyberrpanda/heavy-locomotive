49b146016e56c6766c98386effd81d482aff0df4
Balogun Ithiel <ithielb9@gmail.com>
2025-01-27 21:45:05 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Form from "./components/form"
+
+function App() {
+  return (
+   <>
+     <div className="intro">
+      <p className="intro__text"></p>
+     </div>
+
+    <Form/>
+   </>
+  
+  )
+
+}
+
+export default App
