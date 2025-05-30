83db223f7d2552e33c5001983bb5e2ddd417c958
Balogun Ithiel <ithielb9@gmail.com>
2025-05-30 23:40:35 +0100

    addition to welcome text in welcome component and accompanying styles for responsiveness

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -4,6 +4,7 @@ export default function Welcome() {
   return (
     <section className="welcome">
       <h3 className="welcome__text">Welcome to Heavy Locomotive</h3>
+      <p className="welcome__sub">your easy access to trucking facilities...</p>
       <Link to="/signup" className="btn btn--yellow btn--animated">
         Truck with Us
       </Link>
